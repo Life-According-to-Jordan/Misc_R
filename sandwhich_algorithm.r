ceb34a26f244952aaed1a055082bac9fdb7e94d9
Jordan Hoehne <33847459+Life-According-to-Jordan@users.noreply.github.com>
2018-04-30 20:05:12 -0500

    Create sandwhich_algorithm.r
    Sandwich algorithm for an interview I had recently

---
+++ sandwhich_algorithm.r
@@ -0,0 +1,115 @@
+cat("Steps to making a Ham and Cheese Sandwhich:
+
+0.  Wash and dry hands
+
+1   In kitchen, locate clean counter space with at least 1' by 1' of useable work area.
+      If clean, next step
+        else, clean the area. 
+
+2.   If 1' x 1' useable work area, next step
+        else, move obstructions to proper location or receptical.
+
+3.   In kitchen, locate required ingrediants for sandwhich: Deli Ham, Deli Cheese, Sliced Bread, and the 
+     Mayo of your choosing.
+
+     If at least 1 slice of deli ham, 1 slice of deli cheese, 2 slices of sliced bread, and desired amount of Mayo, place items in alloted kitchen space and proceed to the next step.
+      
+     Else, go to the nearest grocery store, locate needed items in store, purchase required items, return to kitchen, remove items 
+     for grocery bag and place items next to each other with alloted kitchen counter space.
+
+4.   Sort items in alloted kitchen space from left to right in this order: sliced bread, mayo, deli ham, and 
+     deli cheese.
+
+5.   Gather clean utensils need to prepare sandwhich: plate (paper or ceramic), knife, napkin.
+
+     If utensils are clean, next step.
+         else, clean utensils to desired level of cleanliness.
+
+6. With sorted items in allocated kitchen space, place clean utensils in available area in kitchen space.
+
+7. With sliced bread, locate easiest access to sliced bread while maintaining integrity of sliced bread packaging. 
+
+8. With easist access point to sliced bread, open bread container maintaining integrity of sliced bread packaging.
+
+     If twisty-tie, unwind tie until no restriction on bread, proceed to next step.
+        else, remove obstruction to opening in bread.
+
+9. With bread opened, insert hand into sliced bread packaging opening until you feel bread.
+
+10. With hands on bread, gently grab 2 slices of sliced bread and remove from packaging and place wide side flat next 
+    to each other on plate in work space.    
+    
+11. With desired mayo, firmly grab base of mayo while in an upright position with mayo lid facing up.
+
+12. With non-dominate hand and unscrew lid of mayo and gently place lid in available work space area. 
+    
+13. With desired mayo in non-dominant hand, inspect mayo packaging for barrier to mayo.
+
+      If no barrier to mayo, next step 
+         else, remove barrier and discard
+
+14. With desired mayo in non-dominant hand, grab knife with dominant hand and dip tip knife into mayo, approximately 
+    0 - 2 inches for desired amount of mayo
+
+15. With knife in mayo at desired amount, remove knife in a scoping motion keeping mayo on knife.
+
+16. With mayo on knife, gently place mayo container in available work area.
+
+17. With knife in hand containing mayo, gently spread mayo on the upward-facing slices of spread, spreading evenly across surface.
+    
+      If desired amount of mayo, next step
+         else, repeat steps 14-17 until desired amount of mayo
+
+18. Place used knife in sink to be cleaned later
+
+19. Grad mayo container and replace lid on container.
+
+       If no feasible amount of mayo is left for later use discard to proper recepticle and proceed to next step 
+           else, place in proper location of your choosing for later use.
+
+19. Gently grab deli meet and locate easiest access to deli ham while maintaining integrity of deli ham packaging. 
+
+20. With easist access point to deli mear, open deli ham container maintaining integrity of deli ham packaging and reach hand 
+    into packaging until you feel the ham touching your fingers.
+
+21. With fingers touching ham, grab desired amount of ham and distribute evenly on top of mayo on the upward facing slices of bread.
+
+       If pleased with the amount of ham on the bread with mayo, seal deli ham packaging and proceed to the next step
+           else, continue to place until desired amount of deli ham is on bread with mayo.
+
+23. If no feasible amount of deli ham is left for later use discard to proper recepticle and proceed to next step 
+       else, place in proper location of your choosing for later use.
+
+24. Next, gently grab deli cheese and locate easiest access to deli cheese while maintaining integrity of deli cheese packaging. 
+
+25. With easist access point to deli cheese, open deli cheese container maintaining integrity of deli cheese packaging and reach hand 
+    into packaging until you feel the cheese touching your fingers.
+    
+26. With fingers touching the cheese, remove cheese from packaging.
+
+27. Gently grab cheese and ply top layer of cheese from cheese stack and place evenly on top of the deli meat.
+
+28. If pleased with the amount of cheese on the bread with mayo and deli ham and proceed to the next step 
+       else, continue to place deli cheese until desired amount of deli cheese is on bread with mayo and deli ham.
+
+29. When done with cheese, put any remaining cheese from cheese stack into deli cheese packaging
+    
+       If no feasible amount of deli cheese is left for later use discard to proper recepticle and proceed to next step 
+         else, place deli cheese packaging in proper location of your choosing for later use.
+
+30. Check kitchen counter for used items and place containers in proper locations.
+
+31. With plate containing bread with mayo and ham and cheese gently pick up either side of sandwhich contents maintaining 
+    the integrity of the sandwhich and order of the sandwhich.
+
+32. Gently and quickly lay the chosen side of sandwhich on other side, with the cheese facing down and the side of bread
+    with no ingrediants facing up.
+
+33. Move sandwhich igrediants as needed for aesthetic pleasure ensuring near equal amounts of ingrediants for each bite into the sandwhich.
+
+34. Eat sandwhich until full then discard remaining portions of sandwhich to proper recepticle.
+
+35. Clean kitchen area and utensils to your satisfaction.
+
+36. Stop ")
+
